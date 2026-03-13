f5f8e23cf8329bafbd6adf5b470737b1beb84978
andrijanikic23 <andrijanikic001@gmail.com>
2026-03-13 16:35:39 +0100

    Welcome and result routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\ExchangeRateController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/welcome', [ExchangeRateController::class, 'index'])->name('welcome');
+
+Route::get('/result', [ExchangeRateController::class, 'getRequestedRate'])->name('rate.requested');
